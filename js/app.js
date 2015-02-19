4d9fdfbc90223d86a03e0e915af7ad1dff2c29bf
Ludovic Viaud <ludovic.viaud@gmail.com>
2015-02-19 08:56:25 +0100

    player ok

--- js/app.js
+++ js/app.js
@@ -1,35 +1,100 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// Class for drawable objects
+var Drawable = function (sprite, x, y) {
+    this.sprite = sprite;
+    this.x = x === undefined ? 0 : x;
+    this.y = y === undefined ? 0 : y;
+};
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+// Place the drawable at a position
+Drawable.prototype.setPosition = function(x, y) {
+    this.x += x;
+    this.y += y;
+};
+
+// Move the drawable
+Drawable.prototype.move = function(xOffset, yOffset) {
+    this.x += xOffset;
+    this.y += yOffset;
+};
+
+// Draw the drawable
+Drawable.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+
+// Enemies our player must avoid : inherite Drawable
+var Enemy = function() {
+    Drawable.call(this, 'images/enemy-bug.png');
+    // Speed of the enemy (in pixels per second)
+    this.speed = 0;
+    this.start();
+};
+
+// Inherit Drawable
+Enemy.prototype = Object.create(Drawable.prototype);
+
+// Randomly place the enemy in one of the 3 possibles starting positions
+// with a random speed
+Enemy.prototype.start = function() {
+    this.setPosition(-101, 83*(Math.random()%3+1)-30);
+    this.speed = Math.random() % 300 + 200;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    this.move(this.speed * dt, 0);
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+
+// The player : inherits Drawable
+var Player = function() {
+    // Call parent constructor and place him at start
+    Drawable.call(this, 'images/char-boy.png');
+    this.start();
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Inherit Drawable
+Player.prototype = Object.create(Drawable.prototype);
 
+// Place the player at starting point
+Player.prototype.start = function() {
+    this.setPosition(101*2, 83*5-30);
+};
+
+// React to user inputs
+Player.prototype.handleInput = function(direction) {
+    switch(direction) {
+        case 'up':
+            if(this.y > 83-30) {this.move(0, -83);}
+            break;
+        case 'right':
+            if(this.x < 101*4) {this.move(101, 0);}
+            break;
+        case 'down':
+            if(this.y < 83*5-30) {this.move(0, 83);}
+            break;
+        case 'left':
+            if(this.x > 0) {this.move(-101, 0);}
+            break;
+    }
+};
+
+
+
+// Check collision
+function checkCollisions() {
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
